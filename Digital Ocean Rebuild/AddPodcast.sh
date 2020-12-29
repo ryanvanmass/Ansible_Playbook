59bdab6288d20a7d73724eb75ddee071f9968aaa
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2020-12-29 10:32:41 -0500

    Continued Dev

--- Digital Ocean Rebuild/AddPodcast.sh
+++ Digital Ocean Rebuild/AddPodcast.sh
@@ -7,23 +7,23 @@ echo "What is the RSS Feed for the Podcast?"
 read RSSFeed
 
 ### Create Podcast Folder ###
-mkdir /home/ryan/Podcasts/$PodcastName
-mkdir /home/ryan/Podcasts/$PodcastName/.upodder
+mkdir /root/Podcasts/$PodcastName
+mkdir /root/Podcasts/$PodcastName/.upodder
 
 ### Copy upodder template files to Podcast Folder and Configures them###
-cp /home/ryan/Podcasts/.Template/* /home/ryan/Podcasts/$PodcastName/.upodder
+cp /root/Podcasts/.Template/* /root/Podcasts/$PodcastName/.upodder
 
-echo $RSSFeed >> /home/ryan/Podcasts/$PodcastName/.upodder/subscriptions
+echo $RSSFeed >> /root/Podcasts/$PodcastName/.upodder/subscriptions
 
 ### Creates Automation.sh file ###
-echo "#!/bin/bash" >> /home/ryan/Podcasts/$PodcastName/Automation.sh
+echo "#!/bin/bash" >> /root/Podcasts/$PodcastName/Automation.sh
 
-echo "### Runs upodder ###" >> /home/ryan/Podcasts/$PodcastName/Automation.sh
-echo "upodder -b /mnt/Host/.upodder -p /mnt/Host" >> /home/ryan/Podcasts/$PodcastName/Automation.sh
-echo " " >> /home/ryan/Podcasts/$PodcastName/Automation.sh
+echo "### Runs upodder ###" >> /root/Podcasts/$PodcastName/Automation.sh
+echo "upodder -b /mnt/Host/.upodder -p /mnt/Host" >> /root/Podcasts/$PodcastName/Automation.sh
+echo " " >> /root/Podcasts/$PodcastName/Automation.sh
 
 ### Create Podman Cron Job ###
 crontab -l | { cat; echo "# $PodcastName"; } | crontab -
-crontab -l | { cat; echo "0 0-23 * * * podman run --volume /home/ryan/Podcasts/$PodcastName:/mnt/Host --rm localhost/upodder:v3 sh /mnt/Host/Automation.sh"; } | crontab -
-crontab -l | { cat; echo "0 0-23 * * * mkdir -p /media/ryan/PodcastSync/$PodcastName && mv /home/ryan/Podcasts/$PodcastName/*.mp3* /media/ryan/PodcastSync/$PodcastName"; } | crontab -
+crontab -l | { cat; echo "0 0-23 * * * podman run --volume /root/Podcasts/$PodcastName:/mnt/Host --rm localhost/upodder:v3 sh /mnt/Host/Automation.sh"; } | crontab -
+crontab -l | { cat; echo "0 0-23 * * * mkdir -p /media/ryan/PodcastSync/$PodcastName && mv /root/Podcasts/$PodcastName/*.mp3* /media/ryan/PodcastSync/$PodcastName"; } | crontab -
 crontab -l | { cat; echo " "; } | crontab -
