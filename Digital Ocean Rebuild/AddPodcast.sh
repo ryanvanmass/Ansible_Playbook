c27ce2a9c529d8ab268c4a34568008faf9ce6c3c
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2021-01-02 15:19:53 -0500

    Bug Fixes and Switch from Cron to Systemd Timer

--- Digital Ocean Rebuild/AddPodcast.sh
+++ Digital Ocean Rebuild/AddPodcast.sh
@@ -22,8 +22,46 @@ echo "### Runs upodder ###" >> /root/Podcasts/$PodcastName/Automation.sh
 echo "upodder -b /mnt/Host/.upodder -p /mnt/Host" >> /root/Podcasts/$PodcastName/Automation.sh
 echo " " >> /root/Podcasts/$PodcastName/Automation.sh
 
+### Creates AutomationP2.sh ###
+echo " #!/bin/bash" >> /root/Podcasts/$PodcastName/AutomationP2.sh
+echo " mkdir -p /media/root/PodcastSync/$PodcastName" >> /root/Podcasts/$PodcastName/AutomationP2.sh
+echo " mv /root/Podcasts/$PodcastName/*.mp3* /media/root/PodcastSync/AskNoah" >> /root/Podcasts/$PodcastName/AutomationP2.sh
+echo " rm -rf /root/Podcasts/$PodcastName/*.mp3*" >> /root/Podcasts/$PodcastName/AutomationP2.sh
+
+### Create Systemd Service File ###
+echo "[Unit]" >> /etc/systemd/system/"Podman_$PodcastName.service"
+echo "Description=AskNoahPodman" >> /etc/systemd/system/"Podman_$PodcastName.service"
+
+echo " " >> /etc/systemd/system/"Podman_$PodcastName.service"
+
+echo "[Service]" >> /etc/systemd/system/"Podman_$PodcastName.service"
+echo "Type=oneshot" >> /etc/systemd/system/"Podman_$PodcastName.service"
+echo "ExecStart=podman run --volume /root/Podcasts/AskNoah:/mnt/Host --rm docker.io/ryanvanmass/do_upodder:v1 sh /mnt/Host/Automation.sh" >> /etc/systemd/system/"Podman_$PodcastName.service"
+echo "ExecStart= sh /root/Podcasts/AskNoah/AutomationP2.sh" >> /etc/systemd/system/"Podman_$PodcastName.service"
+echo "WorkingDirectory=/" >> /etc/systemd/system/"Podman_$PodcastName.service"
+
+### Create Systemd Timer File ###
+echo "[Unit]" >> /etc/systemd/system/"Podman_$PodcastName.timer"
+echo "Description=AskNoahPodman" >> /etc/systemd/system/"Podman_$PodcastName.timer"
+
+echo " " >> /etc/systemd/system/"Podman_$PodcastName.timer"
+
+echo "[Timer]" >> /etc/systemd/system/"Podman_$PodcastName.timer"
+echo "OnCalendar=hourly" >> /etc/systemd/system/"Podman_$PodcastName.timer"
+echo "Persistent=true" >> /etc/systemd/system/"Podman_$PodcastName.timer"
+
+echo " " >> /etc/systemd/system/"Podman_$PodcastName.timer"
+
+echo "[Install]" >> /etc/systemd/system/"Podman_$PodcastName.timer"
+echo "WantedBy=timers.target" >> /etc/systemd/system/"Podman_$PodcastName.timer"
+
+### Reload systemd daemeon and start the timer ###
+systemctl daemon-reload
+systemctl enable --now $PodcastName.timer
+
+
 ### Create Podman Cron Job ###
-crontab -l | { cat; echo "# $PodcastName"; } | crontab -
-crontab -l | { cat; echo "0 0-23 * * * podman run --volume /root/Podcasts/$PodcastName:/mnt/Host --rm docker.io/ryanvanmass/do_upodder:v1 sh /mnt/Host/Automation.sh"; } | crontab -
-crontab -l | { cat; echo "0 0-23 * * * mkdir -p /media/root/PodcastSync/$PodcastName && mv /root/Podcasts/$PodcastName/*.mp3* /media/root/PodcastSync/$PodcastName"; } | crontab -
-crontab -l | { cat; echo " "; } | crontab -
+#crontab -l | { cat; echo "# $PodcastName"; } | crontab -
+#crontab -l | { cat; echo "0 0-23 * * * podman run --volume /root/Podcasts/$PodcastName:/mnt/Host --rm docker.io/ryanvanmass/do_upodder:v1 sh /mnt/Host/Automation.sh"; } | crontab -
+#crontab -l | { cat; echo "0 0-23 * * * mkdir -p /media/root/PodcastSync/$PodcastName && mv /root/Podcasts/$PodcastName/*.mp3* /media/root/PodcastSync/$PodcastName"; } | crontab -
+#crontab -l | { cat; echo " "; } | crontab -
